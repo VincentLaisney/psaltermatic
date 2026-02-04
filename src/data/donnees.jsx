da09ace8f653bb5b479704a6772fce5eada0a740
Vincent Laisney <vincent.laisney@biblico.it>
2026-02-04 09:49:30 +0100

    Divide the Psalms in verses (this commit should be older than the preceding one)

--- src/data/donnees.jsx
+++ src/data/donnees.jsx
@@ -48,7 +48,7 @@ const divinum = ["Divínum auxílium maneat semper nobíscum.",
 const psaumes = {
     118_8: ["Pórtio mea, Dómine, ★ dixi custodíre legem tuam. ",
 "Deprecátus sum fáciem tuam in toto corde meo : ★ miserére mei secúndum elóquium tuum.",
-"Cogitávi vias meas : ★ et convér ti pedes meos in testimónia tua.",
+"Cogitávi vias meas : ★ et convérti pedes meos in testimónia tua.",
 "Parátus sum, et non sum turbátus : ★ ut custódiam mandáta tua.",
 "Funes peccatórum circumpléxi sunt me : ★ et legem tuam non sum oblítus.",
 "Média nocte surgébam ad confiténdum tibi, ★ super iudícia iustificatiónis tuæ.",
